a1dd35adc5fece7663d182571b183b2626dccfe8
HarshitProF <121898573+HarshitProF@users.noreply.github.com>
2023-01-07 20:17:04 +0530

    Update routes.py

--- market/routes.py
+++ market/routes.py
@@ -5,7 +5,7 @@ from bot import bot
 API_KEY='5856492498:AAEdrY7BZDG6_Bsny3oZ1X7BSfctZmLc8j8'
 url='/'+API_KEY
 @app.route(url,methods=['POST'])
-def handle(request):
+def handle():
     if request.method=="POST":
         json_string=request.get_data().decode('utf-8')
         update=Update.de_json(json_string)
