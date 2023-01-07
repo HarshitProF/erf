9c524bbe352e942e2fa668f206b575231e4a5339
HarshitProF <121898573+HarshitProF@users.noreply.github.com>
2023-01-07 20:24:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,6 @@ from telebot import TeleBot
 API_KEY='5856492498:AAEdrY7BZDG6_Bsny3oZ1X7BSfctZmLc8j8'
 bot=TeleBot(API_KEY)
 
-@bot.message_handler(func=lambda :True)
+@bot.message_handler(func=lambda message:True)
 def reply(message):
     bot.send_message(message.from_user.id ,"hello")
