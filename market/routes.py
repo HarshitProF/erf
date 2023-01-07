f613e3725de28fdf53c6be107efc0d962fd84f38
HarshitProF <121898573+HarshitProF@users.noreply.github.com>
2023-01-07 21:20:39 +0530

    Update routes.py

--- market/routes.py
+++ market/routes.py
@@ -8,10 +8,7 @@ url='/'+API_KEY
 def handle():
     if request.method=="POST":
         json_string=request.get_data().decode('utf-8')
-        print(json_string)
-        update=Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return json_string
+        print(json_string['text'])
 @app.route('/')
 def welcome():
     return "hello,home"
