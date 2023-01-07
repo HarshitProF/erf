e17899f0f873d86d1c78469d3bf8cfe9069d05e2
HarshitProF <121898573+HarshitProF@users.noreply.github.com>
2023-01-07 23:05:00 +0530

    Update routes.py

--- market/routes.py
+++ market/routes.py
@@ -11,21 +11,27 @@ def handle():
         json_string=request.get_data().decode('utf-8')
         update=Update.de_json(json_string)
         bot.process_new_updates([update])
-        json1=json.loads(json_string)
-        message=json1['message']['text']
-        messa=message.split("\n")
-        data={
+        bot.send_message(message['message']['from']['id'],text="hi")
+        return "done"
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    text=str(message.text)
+    messa=text.split("\n")
+    data={
         "dat":messa[0],
         "pair":messa[2].split(":")[1],
         "type":messa[3],
         "leverage":messa[4].split(":")[1],
         "entry":messa[5].split(":")[1],
-        "tagets":[float(messa[7].split(":")[1]),float(messa[8].split(":")[1]),float(messa[9].split(":")[1]),float(messa[10].split(":")[1]),float(messa[11].split(":")[1]),float(messa[12].split(":")[1]),float(messa[13].split(":")[1])],
+        "tagets":[float(messa[7].split(":")[1]),float(messa[8].split(":")[1]),float(messa[9].split(":")[1]),float(messa[10].split(":")[1]),float(messa[11].split(":")[1]),float(messa[12].split(":")[1]),(messa[13].split(":")[1])],
         "stoploss": messa[15].split(":")[1]
-        }
-        print(data)
-        bot.send_message(message['message']['from']['id'],text="hi")
-        return "done"
+    }
+    message1=f"✨{data['pair']}\n\n🎗 Trade Type={data['type']}\n\n💫 Leverage={data['leverage']}\n\n⚡️ Entry={data['entry']}\n\n❌ StopLoss={data['stoploss']}\n\n❎ Take profit={data['tagets']}"
+    message2=f"📍 {data['pair']}\n\n🏹 Signal Type:- {data['type']}\n\n💫Leverage: {data['leverage']}\n\n👉 Entry Targets:- {data['entry']}\n\n🎯 Profit Targets:\n1) {data['tagets'][0]}\n2) {data['tagets'][1]}\n3) {data['tagets'][2]}\n4) {data['tagets'][3]}\n5) {data['tagets'][4]}\n6) {data['tagets'][5]}\n7) {data['tagets'][6]}\n\n🛑 Stop Target: {data['stoploss']} "
+    message3=f"⚡️💫 {data['pair']} 💫⚡️\n\n[{data['type']}]:{data['entry']}\n\n✨🎯 TARGETS ✨🎯\n\n1.Goal👉 {data['tagets'][0]}\n2.Goal👉 {data['tagets'][1]}\n3.Goal👉 {data['tagets'][2]}\n4.Goal👉 {data['tagets'][3]}\n5.Goal👉 {data['tagets'][4]}\n6.Goal👉 {data['tagets'][5]}\n7.Goal👉 {data['tagets'][6]}\n\nSL🛑:- {data['stoploss']}\n\n🎗 LEVERAGE:- {data['leverage']}"
+    bot.send_message(message.from_user.id ,text=message1)
+    bot.send_message(message.from_user.id ,text=message2)
+    bot.send_message(message.from_user.id ,text=message3)
 @app.route('/')
 def welcome():
     return "hello,home"
