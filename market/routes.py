71464229ceb94e60aceb7871750147168942bc5e
HarshitProF <121898573+HarshitProF@users.noreply.github.com>
2023-01-07 21:39:08 +0530

    Update routes.py

--- market/routes.py
+++ market/routes.py
@@ -2,14 +2,16 @@ from market import app
 from flask import request,jsonify
 from telebot.types import Update
 from bot import bot
+import json
 API_KEY='5856492498:AAEdrY7BZDG6_Bsny3oZ1X7BSfctZmLc8j8'
 url='/'+API_KEY
 @app.route(url,methods=['POST'])
 def handle():
     if request.method=="POST":
         json_string=request.get_data().decode('utf-8')
-        json1=jsonify(json_string)
-        print(json1['text'])
+        
+        json1=json.loads(json_string)
+        print(json1["text"])
         print(json_string['text'])
 @app.route('/')
 def welcome():
